302bb024215a94bf7d7d977c58660cc3e52ab82f
gangside <x_hojun@naver.com>
2020-05-16 21:17:20 +0900

    upload morm library

---
+++ 01_MormLibrary/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    public static T[] ShuffleArray<T>(T[] array, int seed)
+    {
+        System.Random prng = new System.Random(seed);
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int randomIndex = prng.Next(i, array.Length);
+            T tempItem = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = tempItem;
+        }
+
+        return array;
+    }
+
+    public static List<T> ShuffleArray<T>(List<T> array, int seed)
+    {
+        System.Random prng = new System.Random(seed);
+
+        for (int i = 0; i < array.Count - 1; i++)
+        {
+            int randomIndex = prng.Next(i, array.Count);
+            T tempItem = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = tempItem;
+        }
+
+        return array;
+    }
+}
